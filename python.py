6a651d12f8410931abe2b9496090a67ad8cebf57
zahir <alijuanan@gmail.com>
2025-09-23 22:15:58 -0500

    segundo commit

---
+++ python.py
@@ -0,0 +1,6 @@
+palabra = input("ingresa una palabra \n")
+contador = 0
+for letra in palabra:
+    if letra.lower() == "a" or letra.lower() == "e"or letra.lower() == "i"or letra.lower() == "o" or letra.lower() == "u":
+        contador+=1
+print(f"la palabra '{palabra}' tiene {contador} vocales")
